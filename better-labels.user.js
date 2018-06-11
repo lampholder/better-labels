c23521f57d7bc934e067e2f89a59400a32c1b113
Tom Lant <lampholder@gmail.com>
2018-06-11 12:15:01 +0100

    BOOM

---
+++ better-labels.user.js
@@ -0,0 +1,127 @@
+// ==UserScript==
+// @name        Better labels.
+// @include     /^https://github\.com/.+/.+/issues/[0-9]+$/
+// @require     http://code.jquery.com/jquery-latest.js
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function blackOrWhite(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        var red = parseInt(result[1], 16);
+        var green = parseInt(result[2], 16);
+        var blue = parseInt(result[3], 16);
+
+        if ((red*0.299 + green*0.587 + blue*0.114) > 186) {
+            return '#000000';
+        }
+        else {
+            return '#ffffff';
+        }
+    };
+
+    function compareLabels(labelA, labelB) {
+        if(labelA.fields.category < labelB.fields.category) return -1;
+        if(labelA.fields.category > labelB.fields.category) return 1;
+        if(labelA.name < labelB.name) return -1;
+        if(labelA.name > labelB.name) return 1;
+        return 0;
+    };
+
+    jQuery(document).ready(function() {
+
+        jQuery('#partial-discussion-sidebar').prepend('<div class="discussion-sidebar-item">' +
+                                                        '<button type="button" id="bl-button" class="discussion-sidebar-heading discussion-sidebar-toggle ' +
+                                                        'js-menu-target" data-hotkey="b">' +
+                                                          '<svg class="octicon octicon-gear" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M14 8.77v-1.6l-1.94-.64-.45-1.09.88-1.84-1.13-1.13-1.81.91-1.09-.45-.69-1.92h-1.6l-.63 1.94-1.11.45-1.84-.88-1.13 1.13.91 1.81-.45 1.09L0 7.23v1.59l1.94.64.45 1.09-.88 1.84 1.13 1.13 1.81-.91 1.09.45.69 1.92h1.59l.63-1.94 1.11-.45 1.84.88 1.13-1.13-.92-1.81.47-1.09L14 8.75v.02zM7 11c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"></path></svg>' +
+                                                          'Better Labels' +
+                                                        '</button>' +
+                                                        '<div id="bl-selector" class="select-menu label-select-menu"> ' +
+                                                          '<div class="select-menu-modal-holder" > ' +
+                                                            '<div class="select-menu-modal">' +
+                                                              '<div class="select-menu-header">' +
+                                                                '<div class="select-menu-title">' +
+                                                                  'Apply better labels to this issue' +
+                                                                '</div>' +
+                                                              '</div>' +
+
+                                                              '<div class="select-menu-filters">' +
+                                                                '<div class="select-menu-text-filter">' +
+                                                                  '<input type="text" id="label-filter-field" class="form-control js-label-filter-field js-filterable-field js-navigation-enable" placeholder="Filter or create labels" autocomplete="off">' +
+                                                                '</div>' +
+                                                              '</div>' +
+
+                                                              '<div class="select-menu-list" id="bl-labels">' +
+                                                              '</div>' +
+                                                            '</div>' +
+                                                          '</div>' +
+                                                        '</div>' +
+                                                        '<div id="bl-issue-labels" class="labels css-truncate"></div>' +
+                                                        '</div>');
+        var button = jQuery('#bl-button');
+        button.click(function() {
+            jQuery('#bl-selector').toggleClass('active');
+        });
+
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: 'https://3iplbb6gp0.execute-api.us-east-1.amazonaws.com/dev/labels',
+            onload: function(response) {
+                var labels = JSON.parse(response.responseText);
+                labels.sort(compareLabels);
+
+                labels.forEach(function(label) {
+                    var bgcolor = label.fields.color || 'lightgray';
+                    var fgcolor = blackOrWhite(bgcolor);
+                    var name = label.fields.displayName || label.name;
+                    var description = label.fields.description || '';
+                    var link = jQuery('#bl-labels').append('<div class="select-menu-item">' +
+                                                             '<div class="select-menu-item-text">' +
+                                                               '<div class="float-left color mr-2" style="margin-top: 2px; background-color: ' + bgcolor + '"></div>' +
+                                                               '<div>' +
+                                                                 '<span class="name">' + name + '</span>' +
+                                                                 '<div class="description d-block text-gray css-truncate-target m-0">' + description + '</div>' +
+                                                               '</div>' +
+                                                             '</div>' +
+                                                           '</div>');
+                    link.click(function() {
+                        GM_xmlhttpRequest({
+                            method: 'POST',
+                            headers: {"Content-Type": "application/json"},
+                            url: 'https://3iplbb6gp0.execute-api.us-east-1.amazonaws.com/dev/' + window.location.pathname + '/labels',
+                            data: JSON.stringify([label.id])
+                        });
+                    });
+                });
+            },
+            onerror: function() {
+                console.log('derp');
+                alert('zxczxzzzzz');
+            }
+        });
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: 'https://3iplbb6gp0.execute-api.us-east-1.amazonaws.com/dev/' + window.location.pathname + '/labels',
+            // url: 'https://3iplbb6gp0.execute-api.us-east-1.amazonaws.com/dev/labels',
+            onload: function(response) {
+                var labels = JSON.parse(response.responseText).labels;
+                labels.sort(compareLabels);
+
+                labels.forEach(function(label) {
+                    var bgcolor = label.fields.color || 'lightgray';
+                    var fgcolor = blackOrWhite(bgcolor);
+                    var name = label.fields.displayName || label.name;
+                    jQuery('#bl-issue-labels').append('<a class="sidebar-labels-style box-shadow-none width-full d-block ' +
+                                                                'IssueLabel v-align-text-top" style="background-color: ' + bgcolor + '; ' +
+                                                                'color: ' + fgcolor + '; margin-top: 3px;">' + name + '</a>');
+                });
+            },
+            onerror: function() {
+                console.log('derp');
+                alert('zxczxzzzzz');
+            }
+        });
+    });
+})();
---
+++ better-labels/app.py
@@ -0,0 +1,405 @@
+# app.py
+
+import os
+import json
+import uuid
+
+import boto3
+
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+"""
+What does this thing look like?
+
+GET     mount/lampholder/concorde/issues/1234/labels
+
+{
+    'labels': [
+        {
+            'id': 'uuid-uuid-uuid-uuid-uuid-uuid',
+            'name': 'bug',
+            'uri': 'mount/labels/uuid-uuid-uuid-uuid-uuid-uuid',
+            'fields': {
+                'color': '#FFAB23',
+                'type': 'defect'
+            }
+        },
+        {
+            'id': 'uuid-uuid-uuid-uuid-uuid-uuid',
+            'name': 'maintenance',
+            'uri': 'mount/labels/uuid-uuid-uuid-uuid-uuid-uuid',
+            'fields': {
+                'color': '#FFAB23',
+                'type': 'maintenance'
+            }
+        },
+    ]
+}
+
+GET     mount/lampholder/concorde/issues/1234/labels/uuid-uuid-uuid-uuid-uuid-uuid
+
+{
+    'id': 'uuid-uuid-uuid-uuid-uuid-uuid',
+    'name': 'bug',
+    'fields': {
+        'color': '#FFAB23',
+        'type': 'defect'
+    }
+}
+
+GET     mount/labels/uuid-uuid-uuid-uuid-uuid-uuid
+
+{
+    'id': 'uuid-uuid-uuid-uuid-uuid-uuid',
+    'name': 'bug',
+    'fields': {
+        'color': '#FFAB23',
+        'type': 'defect'
+    }
+}
+
+"""
+
+"""
+
+1.1 GET    mount/labels
+        -> Return the full representation of all the labels
+
+1.2 PUT    mount/labels/<uuid>
+        -> (Over)write a representation of a label with the specified uuid
+
+1.3 GET    mount/labels/<uuid>
+        -> Return the full representation of a single label
+
+1.4 POST   mount/labels
+        -> Write a representation of a label with a system-provided uuid
+
+1.5 DELETE mount/labels/<uuid>
+        -> Delete the label with the specified uuid
+
+1.6 DELETE mount/labels [NOT IMPLEMENTED]
+        -> Delete all the labels
+
+1.7 PUT    mount/labels
+        -> Replace all of the labels with a fresh representation
+
+
+2.1 GET    mount/lampholder/concorde/issues/1234/labels
+        -> Return the full json representation of the indicated label
+
+2.2 PUT    mount/lampholder/concorde/issues/1234/labels
+        -> Submit a list of label ids that will replace the existing list [NOT
+           IMPLEMENTED]
+
+2.3 POST   mount/lampholder/concorde/issues/1234/labels
+        -> Submit a list of label ids that will add to the existing list
+
+2.4 DELETE mount/lampholder/concorde/issues/1234/labels
+        -> Delete all of the labels associated with an issue
+
+2.5 DELETE mount/lampholder/concorde/issues/1234/labels/<uuid>
+        -> Delete just that one label from the issue's list
+
+2.6 PATCH  mount/lampholder/concorde/issues/1234/labels
+        -> Add all of the labels specified (by id) in addLabelIds, then
+           delete all of the labels specified (by id) in removelabelIds
+
+"""
+
+
+
+
+
+class DAO:
+    """
+    This DAO object is responsible for:
+        - managing all the connections to the db
+        - marshalling dynamodb objects into standard python objects
+
+    As a general rule, every non-get methods should return their id.
+    """
+
+    ISSUES_TABLE = os.environ['ISSUES_TABLE'] if 'ISSUES_TABLE' in os.environ else 'issues-table-dev'
+    LABELS_TABLE = os.environ['LABELS_TABLE'] if 'LABELS_TABLE' in os.environ else 'labels-table-dev'
+
+    def __init__(self, url_stem, region='us-east-1'):
+        self._url_stem = url_stem
+        self._client = boto3.client('dynamodb', region_name = region)
+
+
+    def _item_to_label(self, item):
+        return {
+            'id': item.get('id').get('S'),
+            'name': item.get('name').get('S'),
+            # 'location': self._url_stem + '/labels/' + item.get('id').get('S'),
+            'fields': json.loads(item.get('fields').get('S'))
+        }
+
+
+    def get_labels(self):
+        """Returns fully-hydrated label objects"""
+        resp = self._client.scan(TableName=self.LABELS_TABLE)
+        return [self._item_to_label(item) for item in resp['Items']]
+
+
+    def get_label(self, id):
+        resp = self._client.get_item(
+            TableName=self.LABELS_TABLE,
+            Key={
+                'id': {'S': id}
+            }
+        )
+
+        if 'Item' not in resp:
+            return None
+
+        return self._item_to_label(resp['Item'])
+
+    """
+    {
+        'id': 'uuid-uuid-uuid-uuid-uuid-uuid',
+        'name': 'bug',
+        'location': 'https://example.com/labels/uuid-uuid-uuid-uuid-uuid-uuid',
+        # location is generated, not stored
+        'fields': {
+            'color': '#FFAB23',
+            'type': 'defect'
+        }
+    }
+    """
+    def set_label(self, id, name, fields):
+        label = {
+            'id': {'S': id},
+            'name': {'S': name},
+            'fields': {'S': json.dumps(fields)}
+        }
+
+        self._client.put_item(
+            TableName=self.LABELS_TABLE,
+            Item=label
+        )
+
+        return id
+
+    def delete_all_labels(self):
+        """
+        FIXME: This is not the right way to do this
+        """
+        labels = self.get_labels()
+        for label in labels:
+            self.delete_label(label['id'])
+
+    def delete_label(self, id):
+        self._client.delete_item(
+            TableName=self.LABELS_TABLE,
+            Key={
+                'id': {'S': id}
+            }
+        )
+
+        return id
+
+
+    def get_issue_labels(self, issue_path):
+        label_ids = self.get_issue_label_ids(issue_path)
+
+        return {
+            'path': issue_path,
+            # 'location': self._url_stem + '/' + issue_path + '/labels',
+            'labels': [label for label in [self.get_label(label_id) for label_id in
+                                           label_ids]
+                       if label is not None]
+        }
+
+
+    def get_issue_label_ids(self, issue_path):
+        resp = self._client.get_item(
+            TableName=self.ISSUES_TABLE,
+            Key={
+                'path': {'S': issue_path}
+            }
+        )
+
+        if 'Item' not in resp:
+            return []
+
+        item = resp['Item']
+        return item['labelIds']['SS'] if 'labelIds' in item else []
+
+
+    def set_issue_label_ids(self, issue_path, label_ids):
+        item = {
+            'path': {'S': issue_path}
+        }
+
+        real_label_ids = [label['id'] for label in self.get_labels()]
+
+        new_label_ids = [label_id for label_id in label_ids
+                         if label_id in real_label_ids]
+
+        if len(new_label_ids) > 0:
+            item['labelIds'] = {'SS': list(set(new_label_ids))}
+
+        put = self._client.put_item(
+            TableName=self.ISSUES_TABLE,
+            Item=item
+        )
+
+        return issue_path
+
+
+dao = DAO(url_stem='https://localhost:5000')
+
+# Convenience methods:
+
+def if_found(value):
+    if value:
+        return jsonify(value)
+    else:
+        return jsonify({'error': 'Not found'}), 404
+
+
+def rehydrate_labels(label_ids):
+    return [dao.get_label(label_id) for label_id in label_ids]
+
+
+# Routed methods:
+
+@app.route('/labels', methods=['GET'])
+def get_labels():
+    """
+    1.1 GET    mount/labels
+            -> Return the full representation of all the labels
+    """
+    return if_found(dao.get_labels())
+
+@app.route('/labels/<string:id>', methods=['PUT'])
+def put_label(id):
+    """
+    1.2 PUT    mount/labels/<uuid>
+            -> (Over)write a representation of a label with the specified uuid
+    """
+    label_id = dao.set_label(id=id,
+                             name=request.json['name'],
+                             fields=request.json['fields'])
+
+    return jsonify(dao.get_label(label_id))
+
+@app.route('/labels', methods=['PUT'])
+def put_labels():
+    """
+    1.7 PUT    mount/labels
+            -> Replace all of the labels with a fresh representation
+    """
+    dao.delete_all_labels()
+    for label in request.json:
+        if 'id' not in label or not label['id']:
+            label['id'] = str(uuid.uuid4())
+        dao.set_label(id=label['id'],
+                      name=label['name'],
+                      fields=label['fields'])
+    return if_found(dao.get_labels())
+
+@app.route('/labels/<string:id>', methods=['GET'])
+def get_label(id):
+    """
+    1.3 GET    mount/labels/<uuid>
+            -> Return the full representation of a single label
+    """
+    return if_found(dao.get_label(id))
+
+@app.route('/labels', methods=['POST'])
+def post_label():
+    """
+    1.4 POST   mount/labels
+            -> Write a representation of a label with a system-provided uuid
+    """
+    label_id = dao.set_label(id=str(uuid.uuid4()),
+                             name=request.json['name'],
+                             fields=request.json['fields'])
+
+    return jsonify(dao.get_label(label_id))
+
+@app.route('/labels/<string:id>', methods=['DELETE'])
+def delete_label(id):
+    """
+    1.5 DELETE mount/labels/<uuid>
+            -> Delete the label with the specified uuid
+    """
+    dao.delete_label(id)
+    return jsonify(dao.get_label(id))
+
+
+@app.route('/<string:repo>/<string:project>/issues/<int:issue_number>/labels',
+           methods=['GET'])
+def get_issue_labels(repo, project, issue_number):
+    """
+    2.1 GET    mount/lampholder/concorde/issues/1234/labels
+            -> Return the full json representation of the indicated label
+    """
+    issue_path = '%s/%s/issues/%d' % (repo, project, issue_number)
+
+    return jsonify(dao.get_issue_labels(issue_path))
+
+@app.route('/<string:repo>/<string:project>/issues/<int:issue_number>/labels',
+           methods=['POST'])
+def post_issue_labels(repo, project, issue_number):
+    """
+    2.3 POST   mount/lampholder/concorde/issues/1234/labels
+            -> Submit a list of label ids that will add to the existing list
+    """
+    issue_path = '%s/%s/issues/%d' % (repo, project, issue_number)
+
+    new_label_ids = request.json
+
+    current_label_ids = dao.get_issue_label_ids(issue_path)
+
+    revised_label_ids = current_label_ids + new_label_ids
+
+    dao.set_issue_label_ids(issue_path, revised_label_ids)
+
+    return if_found(dao.get_issue_labels(issue_path))
+
+@app.route('/<string:repo>/<string:project>/issues/<int:issue_number>/labels/<string:label_id>',
+           methods=['DELETE'])
+def delete_issue_label(repo, project, issue_number, delete_label_id):
+    """
+    2.5 DELETE mount/lampholder/concorde/issues/1234/labels/<uuid>
+            -> Delete just that one label from the issue's list
+    """
+    issue_path = '%s/%s/issues/%d' % (repo, project, issue_number)
+
+    current_label_ids = dao.get_issue_label_ids(issue_path)
+
+    revised_label_ids = [label_id for label_id in current_label_ids
+                         if label_id != delete_label_id]
+
+    dao.set_issue_label_ids(issue_path, revised_label_ids)
+
+    return if_found(dao.get_issue_labels(issue_path))
+
+@app.route('/<string:repo>/<string:project>/issues/<int:issue_number>/labels',
+           methods=['PATCH'])
+def patch_issue_labels(repo, project, issue_number):
+    """
+    2.6 PATCH  mount/lampholder/concorde/issues/1234/labels
+            -> Add all of the labels specified (by id) in addLabelIds, then
+               delete all of the labels specified (by id) in removelabelIds
+    """
+    add_label_ids = request.json.get('addLabelIds') or []
+    remove_label_ids = request.json.get('removeLabelIds') or []
+
+    issue_path = '%s/%s/issues/%d' % (repo, project, issue_number)
+
+    current_label_ids = dao.get_issue_label_ids(issue_path)
+
+    revised_label_ids = [label_id for label_id in current_label_ids + add_label_ids
+                         if label_id not in remove_label_ids]
+
+    dao.set_issue_label_ids(issue_path, revised_label_ids)
+
+    return if_found(dao.get_issue_labels(issue_path))
+
+if __name__ == "__main__":
+    app.run()
